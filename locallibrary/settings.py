f13a013f3415ab67eb166fe2b3aacd79d715ee4f
Lorenzo Gonzalez <lorencho@gmail.com>
2021-10-27 10:53:32 -0500

    quite pass

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-nu%@k$882s$qu_hjvrvsd1&4fnq&e3$@r0*er4bqveq5qgs9y('
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
